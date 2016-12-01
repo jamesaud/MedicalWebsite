45edc97a1947bab5fe65783a02f20fd1e7c86a5a
jamesaud <jamaudre@indiana.edu>
2016-12-01 14:16:53 -0500

    build changes

--- compose/wordpress-mariadb/docker-entrypoint.sh
+++ compose/wordpress-mariadb/docker-entrypoint.sh
@@ -35,7 +35,7 @@ if [ "$1" = 'mysqld' -a -z "$wantHelp" ]; then
 		mkdir -p "$DATADIR"
 
 		echo 'Initializing database'
-		mysql_install_db --datadir="$DATADIR"
+		mysqld --initialize --datadir="$DATADIR"
 		echo 'Database initialized'
 
 		"$@" --skip-networking &
